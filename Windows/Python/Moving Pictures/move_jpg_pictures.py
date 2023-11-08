00e4c485333edbf791022c132cd92bc21b2c2207
Andrew Dass <dassandrew3@gmail.com>
2023-11-08 18:55:03 -0500

    Update move_jpg_pictures.py
    Updated move_jpg_pictures.py script so shutil.move() command gets the absolute path of the directories with the help of the os module to prevent errors from executing the script. Some spacing issues for the script have also been fixed.

--- Windows/Python/Moving Pictures/move_jpg_pictures.py
+++ Windows/Python/Moving Pictures/move_jpg_pictures.py
@@ -4,7 +4,6 @@ import os
 import glob
 import shutil
 
-
 download_path = os.getcwd()
 str_download_path = str(download_path)
 
@@ -12,16 +11,17 @@ jpg_images = glob.glob("*.JPG")
 picture_JPG_entry = 0
 
 if download_path == r"":
-  # pictures in download folder
-        for i in jpg_images:
-                current_image = jpg_images[picture_JPG_entry]
-                str_current_image = str(current_image)
+    for i in jpg_images:
+        current_image = jpg_images[picture_JPG_entry]
+        str_current_image = str(current_image)
 
-                original_path = str_download_path + "\\" + str_current_image
-                print(original_path)
+        original_path = str_download_path + "\\" + str_current_image
+        print(original_path)
 
-                shutil.move(original_path, r"" + "\\" + str_current_image )
-                # move pictures in new directory
-                picture_JPG_entry += 1
+        shutil.move( os.path.abspath(str_current_image), os.path.abspath(r"") + "\\" + str_current_image )
 
-print("Success. Images moved!")
+        picture_JPG_entry += 1
+
+    print("Success. Images moved!")
+else:
+    print("Not in the same directory. No images moved.")
