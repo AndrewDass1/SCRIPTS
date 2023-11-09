d4f550e7acd769a969f396d6b9bcd0dc76935604
Andrew Dass <dassandrew3@gmail.com>
2023-11-08 19:04:31 -0500

    Update move_png_pictures.py
    Fixed more spacing issues.

--- Windows/Python/Moving Pictures/move_png_pictures.py
+++ Windows/Python/Moving Pictures/move_png_pictures.py
@@ -11,17 +11,17 @@ png_images = glob.glob("*.png")
 picture_png_entry = 0
 
 if download_path == r"":
-    for i in png_images:
-        current_image = png_images[picture_png_entry]
-	str_current_image = str(current_image)
+	for i in png_images:
+		current_image = png_images[picture_png_entry]
+		str_current_image = str(current_image)
 
-	original_path = str_download_path + "\\" + str_current_image		
-	print(original_path)
+		original_path = str_download_path + "\\" + str_current_image		
+		print(original_path)
 
-	shutil.move( os.path.abspath(str_current_image), os.path.abspath(r"") + "\\" + str_current_image )
+		shutil.move( os.path.abspath(str_current_image), os.path.abspath(r"") + "\\" + str_current_image )
 
-	picture_png_entry += 1
+		picture_png_entry += 1
 
-    print("Success. Images moved!")
+	print("Success. Images moved!")
 else:
-    print("Not in the same directory. No images moved.")
+	print("Not in the same directory. No images moved.")
