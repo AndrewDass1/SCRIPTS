5aaceb1b04316c1e5e16351cfc1c447c39f53dcb
Andrew Dass <dassandrew3@gmail.com>
2023-11-08 20:43:47 -0500

    Create move_png_pictures_linux.py
    A script to move .png pictures in Linux. This script is different than the Windows one since Linux uses reverse backslashes or "/" rather than backslashes or / to refer to directories. In the script, the reverse backslashes is used to move picture files to different directories.

---
+++ Linux/Python/Moving Pictures/move_png_pictures_linux.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import shutil
+
+download_path = os.getcwd()
+str_download_path = str(download_path)
+
+png_images = glob.glob("*.png")
+picture_png_entry = 0
+
+if download_path == r"":
+	for i in png_images:
+		current_image = png_images[picture_png_entry]
+		str_current_image = str(current_image)
+
+		original_path = str_download_path + "/" + str_current_image		
+		print(original_path)
+
+		shutil.move( os.path.abspath(str_current_image), os.path.abspath(r"") + "/" + str_current_image )
+
+		picture_png_entry += 1
+
+	print("Success. Images moved!")
+else:
+	print("Not in the same directory. No images moved.")
