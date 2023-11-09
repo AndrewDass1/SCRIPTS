625a31f1c21883ce9fd07573f9bcbb2830620564
Andrew Dass <dassandrew3@gmail.com>
2023-11-08 20:39:32 -0500

    Create move_jpg_pictures_linux.py
    A script to move .jpg files to another directory automatically in Linux. This script is slightly different from Windows since Linux directories uses a reverse backslash or "/" to refer to their directories.

---
+++ Linux/Python/Moving Pictures/move_jpg_pictures_linux.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import shutil
+
+download_path = os.getcwd()
+str_download_path = str(download_path)
+
+jpg_images = glob.glob("*.jpg")
+picture_JPG_entry = 0
+
+if download_path == r"":
+    for i in jpg_images:
+        current_image = jpg_images[picture_JPG_entry]
+        str_current_image = str(current_image)
+
+        original_path = str_download_path + "/" + str_current_image
+        print(original_path)
+
+        shutil.move( os.path.abspath(str_current_image), os.path.abspath(r"") + "/" + str_current_image )
+
+        picture_JPG_entry += 1
+
+    print("Success. Images moved!")
+else:
+    print("Not in the same directory. No images moved.")
