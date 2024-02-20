b6a3ccd1a6ba0fd22820aabb430bdd294aa2cc1a
Andrew Dass <dassandrew3@gmail.com>
2024-02-19 19:52:16 -0500

    Update python_music_player.py
    Work in progress. Implemented a for-loop instead to play all songs and time.sleep() to play each song properly until one song finishes before another song plays. 
    
    Future edit: find the time duration of each song and input it into time.sleep() to replace the "100".

--- Python/Play_Music/python_music_player.py
+++ Python/Play_Music/python_music_player.py
@@ -1,12 +1,13 @@
 import glob
 import random
 import os
+import time
 
 list_of_music_in_directory = glob.glob("*.mp3")
 number_of_music_tracks = len(list_of_music_in_directory)
 
-class Find_Music:
 
+class Find_Music:
     def function():
         list_of_music_in_directory = glob.glob("*.mp3")
         number_of_music_tracks = len(list_of_music_in_directory)
@@ -16,11 +17,10 @@ class Find_Music:
         play_music_track = list_of_music_in_directory[random_selector]
         os.startfile(play_music_track)
 
+        time.sleep(100)
+
         del list_of_music_in_directory[random_selector]
         number_of_music_tracks = len(list_of_music_in_directory)
 
-while number_of_music_tracks > 0:
-    try:
-        print(Find_Music.function())
-        
-        number_of_music_tracks -= 1
+for i in range(0, number_of_music_tracks):
+    print(Find_Music.function())
